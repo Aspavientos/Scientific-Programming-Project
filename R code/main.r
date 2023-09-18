e669b38d94c349b29afb7e67157e9c5dbc925c28
Diego Rodriguez <poyutero@gmail.com>
2023-09-19 01:21:50 +0200

    Some data stuff

--- R code/main.r
+++ R code/main.r
@@ -1,3 +1,20 @@
+# Load packages
+require(ggplot2)
+require(mapview)
+
+# Load data
 setwd("..")
 data = read.csv("Data resource - National Mammal Atlas Project.csv")
-data = subset(data, select = c(-Start.date, -Vitality))
+data = subset(data, select = c(-Vitality))
+data = data[-126566,]
+
+
+# Initial diagnostics
+year_hist = hist(data$Start.date.year,
+                 main = "Sighting year histogram",
+                 xlab = "Year")
+
+# Plot sightings
+mapview(data[data$Common.name == "Roe Deer",], 
+        xcol = "Longitude..WGS84.", ycol = "Latitude..WGS84.",
+        label = data$Start.date)
