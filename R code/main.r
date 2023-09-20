1510e503b4f67f0ac9c38e7e301960c4a9866e3c
Diego Rodriguez <poyutero@gmail.com>
2023-09-20 23:47:59 +0200

    some more data cleaning and diagnostics

--- R code/main.r
+++ R code/main.r
@@ -1,18 +1,53 @@
 # Load packages
 require(ggplot2)
 require(mapview)
+require(dplyr)
 
 # Load data
 setwd("..")
-data = read.csv("Data resource - National Mammal Atlas Project.csv")
-data = subset(data, select = c(-Vitality))
-data = data[-126566,]
+data = read.csv("Data resource - National Mammal Atlas Project.csv", stringsAsFactors = TRUE)
+pristine_data = data
 
+# Reformat data
+data = subset(data, select = c(-Vitality, -Country, -Basis.of.record, -Identification.verification.status))
+
+nulls = list(rep(NA, nrow(data))) %>%
+  rep(ncol(data)) %>%
+  setNames(colnames(data)) %>%
+  data.frame()
+
+for (i in 1:ncol(data)){
+  if (class(data[,i]) == 'factor'){
+    nulls[,i] = data[,i] == ''
+  }else{
+    nulls[,i] = is.na(data[,i])
+  }
+} 
+
+data = data[-c(which(nulls$Scientific.name),
+               which(nulls$Start.date),
+               which(nulls$Latitude..WGS84.),
+               which(nulls$Longitude..WGS84.)),]
+
+data$Start.date = as.Date(data$Start.date, format = "%d/%m/%Y")
 
 # Initial diagnostics
-year_hist = hist(data$Start.date.year,
-                 main = "Sighting year histogram",
+year_hist = barplot(data$Start.date.year,
+                 main = "Sighting year histogram, all years",
                  xlab = "Year")
+
+year_hist_ge2010 = hist(data$Start.date.year[data$Start.date.year > 2010],
+                 main = "Sighting year histogram, 2010 onward",
+                 xlab = "Year")
+
+mtext(paste('Removed information:', as.character(round(sum(data$Start.date.year<2010)/nrow(data)*100, 2)), '%'),
+      side = 3)
+
+data = data[-which(data$Start.date.year<2010),]
+
+month_hist = hist(data$Start.date.month,
+                  main = "Sighting month histogram",
+                  xlab = "Month")
 
 # Plot sightings
 mapview(data[data$Common.name == "Roe Deer",], 
