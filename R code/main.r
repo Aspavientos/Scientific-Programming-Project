eae7ec08f908f11cca3d8490e8e21f03bac827e8
Diego Rodriguez <poyutero@gmail.com>
2023-10-01 21:07:19 +0200

    finally fixed

--- R code/main.r
+++ R code/main.r
@@ -28,11 +28,13 @@ nulls = list(rep(NA, nrow(data))) %>%
 
 for (i in 1:ncol(data)){
   if (class(data[,i]) == 'factor'){
+    data[,i] = factor(data[,i], levels = c(levels(data[,i])[-1], levels(data[,i])[1]))
+    levels(data[,i])[levels(data[,i]) == ''] = 'No data'
     nulls[,i] = data[,i] == ''
   }else{
     nulls[,i] = is.na(data[,i])
   }
-} 
+}
 
 data = data[-c(which(nulls$Scientific.name),
                which(nulls$Start.date),
@@ -46,7 +48,7 @@ rm("nulls", "i")
 ## Initial diagnostics
 # Date histograms
 year_hist = ggplot(data, aes(x = Start.date %>% format('%Y'))) +
-  geom_bar(fill = "skyblue", colour = 'black') +
+  geom_bar(stat = 'count', fill = "skyblue", colour = 'black') +
   theme_light() +
   labs(x='Year', y='Frequency') +
   ggtitle('Sighting year histogram', subtitle = 'All years') +
@@ -60,8 +62,10 @@ year_hist_ge2010 = ggplot(data_ge2010, aes(x = Start.date %>% format('%Y'))) +
   geom_bar(fill = "skyblue", colour = 'black') +
   theme_light() +
   labs(x='Year', y='Frequency') +
-  ggtitle('Sighting year histogram', subtitle = paste('2010 onward:', as.character(removed_ge2010), '% samples removed')) +
-  theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+  ggtitle('Sighting year histogram',
+          subtitle = paste('2010 onward:', as.character(removed_ge2010), '% samples removed')) +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
   scale_x_discrete(breaks = as.character(seq(from = 1900, to = 2023, by = 5)))
 
 data = data[-which((data$Start.date %>% format("%Y") %>% as.numeric)<2010),]
@@ -98,33 +102,83 @@ rm(data_ge2010, removed_ge2010)
 
 # Province diagnostics
 province_colors = data.frame(Provinces = levels(data$State.Province),
-                             Fills = c('#FF4F00', '#FFFFFF', '#CF142B', '#5E89C2', '#005EB8', '#00B140'),
-                             Borders = c('#FF4F00', '#CE1124', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'))
-province_colors$Provinces[1] = 'NA'
+                             Fills = c('#FFFFFF', '#CF142B', '#5E89C2', '#005EB8', '#00B140', '#FF4F00'),
+                             Borders = c('#CE1124', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FF4F00'))
 
-province_hist = ggplot(data, aes(x= factor(State.Province))) +
-  geom_bar(fill = province_colors$Fills,
-           colour = province_colors$Borders) +
-  stat_count(geom = "text", size = 3.5, aes(label = after_stat(count)), vjust=-0.25)+
-  theme_light() +
+province_hist = ggplot(data, aes(x= State.Province)) +
+  geom_bar(aes(fill = State.Province,
+               color = State.Province)) +
+  stat_count(geom = "text",
+             size = 3.5,
+             aes(label = after_stat(count)),
+             vjust=-0.25)+
   labs(x= 'Countries', y = 'Frequency') +
   ggtitle('Sightings by Country') +
-  theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))
-
-normprovince_hist = ggplot(data, aes(x = Start.date %>% format('%b') %>% factor(levels = format(ISOdate(2004,1:12,1),"%b")), fill = State.Province)) +
-  geom_bar(position = 'dodge') +
-  theme_light() +
-  labs(x = 'Month', y  = 'Frequency') +
-  ggtitle('Sighting month histogram') +
   theme(plot.title = element_text(hjust = 0.5),
-        legend.title = element_text('Countries'))+
+        legend.position = 'none') +
+  scale_x_discrete(labels = province_colors$Provinces) +
+  scale_fill_manual(values = province_colors$Fills) +
+  scale_color_manual(values = province_colors$Borders)
+
+normprovince_hist = ggplot(data, aes(x = Start.date %>% format('%b') %>% factor(levels = format(ISOdate(2004,1:12,1),"%b")))) +
+  geom_bar(position = 'dodge',
+           aes(fill = State.Province,
+               color = State.Province)) +
+  labs(x = 'Month',
+       y  = 'Frequency') +
+  ggtitle('Sighting month histogram') +
+  theme(plot.title = element_text(hjust = 0.5)) +
   scale_x_discrete(breaks = format(ISOdate(2004,1:12,1),"%b")) +
-  scale_colour_manual(values = province_colors[c('Borders','Fills')],
-                      aesthetics = c('colour', 'fill'),
-                      breaks = levels(data$State.Province),
-                      labels = province_colors$Provinces)
+  scale_fill_manual(name = 'Countries',
+                    labels = province_colors$Provinces,
+                    values = province_colors$Fills) +
+  scale_color_manual(values = province_colors$Borders,
+                     guide = 'none')
 
 normprovince_hist
+
+## MISSING
+
+# Coordiante histograms
+latcoord_hist = ggplot(data, aes(x = Latitude..WGS84.)) +
+  geom_histogram(fill = 'blue', color = 'black') +
+  labs(x = "Latitude", y = "Count") +
+  ggtitle("Latitude histogram") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+longcoord_hist = ggplot(data, aes(x = Longitude..WGS84.)) +
+  geom_histogram(fill = 'red', color = 'black') +
+  labs(x = "Longitude", y = "Count") +
+  ggtitle("Longitude histogram") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+latlong_hist = ggarrange(latcoord_hist, longcoord_hist,
+                      labels = c('Lat', 'Lon'),
+                      ncol = 1, nrow = 2)
+
+uncertcoord_hist = ggplot(data, aes(x = Coordinate.uncertainty..m.)) +
+  geom_histogram(fill = 'cyan', color = 'black') +
+  labs(x = "Uncertainty (m)", y = "Count") +
+  ggtitle("Uncertainty histogram") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+data_biguncert = data[-which((data$Coordinate.uncertainty..m.)>10000),]
+removed_biguncert = round((nrow(data)-nrow(data_biguncert))/nrow(data)*100, 2)
+
+smalluncertcoord_hist = ggplot(data_biguncert, aes(x = Coordinate.uncertainty..m.)) +
+  geom_histogram(fill = 'cyan', color = 'black') +
+  labs(x = "Uncertainty (m)", y = "Count") +
+  ggtitle("Filtered uncertainties histogram",
+          subtitle = paste('Less than 10km:', as.character(removed_biguncert), '% samples removed')) +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
+
+data = data[-which((data$Coordinate.uncertainty..m.)>10000),]
+
+rm(data_biguncert, removed_biguncert)
+
+#
+
 
 # Plot sightings
 
