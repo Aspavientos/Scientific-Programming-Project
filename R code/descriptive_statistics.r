5cc41917294140af2764626b2ca751f2a97d2190
Diego Rodriguez <poyutero@gmail.com>
2023-10-05 01:05:38 +0200

    Finalize groupings method

--- R code/descriptive_statistics.r
+++ R code/descriptive_statistics.r
@@ -15,26 +15,21 @@ data = read.csv("./Data/cleaned_data.csv", stringsAsFactors = TRUE)
 # Supplementary groupings
 data_groupings = list()
 
-test = group_by(data, .data[[group]])
-
-
 createNewGroup = function(group){
   new_grouping = data %>% group_by(.data[[group]])
   # vignette('programming')
   new_grouping_attributes = (new_grouping %>% attributes)$groups
-  
+  data_groupings <<- append(data_groupings, list(groups = new_grouping_attributes))
+  names(data_groupings)[length(data_groupings)] <<- group
 }
 
-new_grouping = data %>% group_by(Start.date)
-new_grouping_attributes = (new_grouping %>% attributes)$groups
-data_groupings = append(data_groupings, list(Start.date = new_grouping_attributes))
+interestgroups = c('Scientific.name',
+                   'Start.date',
+                   'State.Province')
 
-new_grouping = data %>% group_by(Start.date)
-new_grouping_attributes = (new_grouping %>% attributes)$groups
-data_groupings = append(data_groupings, list(Start.date = new_grouping_attributes))
+lapply(interestgroups, createNewGroup)
 
-
-rm(new_grouping, new_grouping_attributes, i)
+rm()
 
 ## Boxplots
-t = group_by(data, Start.date)
+# Mean observations per day
